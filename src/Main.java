f4db37111035069894f4b15d10c72c1938fb2098
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-10-15 22:29:53 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int rand = (int)((Math.random() * 10) + 1);
+        int guess;
+
+        System.out.println("Welcome! Let's play a game.");
+        System.out.println("Try to guess the number I'm thinking of!");
+        do {
+            if (scan.hasNextDouble()) {
+                guess = scan.nextInt();
+                if (guess >= 0) {
+                    break;
+                }
+            }
+            scan.nextLine();
+            System.out.println("Please enter a valid input");
+        } while (true);
+
+        if (guess > rand) {
+            System.out.println("Your guess was too high");
+            System.out.println("The correct answer was: " + rand);
+        } else if (guess < rand) {
+            System.out.println("Your guess was too low");
+            System.out.println("The correct answer was: " + rand);
+        } else {
+            System.out.println("You got it right!");
+            System.out.println("The correct answer was: " + rand);
+        }
     }
 }
